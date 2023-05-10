740b7eb1344bc5f600a26e722008431e3e32efd2
Giuseppe Catalano <giuseppe_catalano@icloud.com>
2023-05-10 12:12:22 +0200

    Added Database connection and data retrieval

--- app/src/main/java/org/progmob/langsupport/MainActivity.kt
+++ app/src/main/java/org/progmob/langsupport/MainActivity.kt
@@ -2,19 +2,26 @@ package org.progmob.langsupport
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Source
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import org.progmob.langsupport.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         auth = Firebase.auth
+        db = Firebase.firestore
+        binding.text.text = "Hello World!"
         setContentView(binding.root)
 
         binding.signUp.setOnClickListener {
@@ -38,5 +45,33 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
         }
+
+        binding.signOut.setOnClickListener {
+            auth.signOut()
+        }
+
+        binding.fetch.setOnClickListener {
+            db.collection("languages")
+                .get()
+                .addOnSuccessListener { result ->
+                    var s = ""
+                    for (document in result) {
+                        s += "${document.id} => ${document.data}\n"
+                    }
+                    binding.text.text = s
+                }
+                .addOnFailureListener { exception ->
+                    Log.w("APP", exception)
+                    binding.text.text = "Errore di retrieval!"
+                }
+        }
+
+        auth.addAuthStateListener { auth ->
+            binding.text.text = "Cambio di auth, utente attuale: ${auth.currentUser?.email}"
+        }
+    }
+
+    companion object {
+        private val TAG = this::class.toString()
     }
 }
